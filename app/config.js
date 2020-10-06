1f5df75c5e89a529868e20abc1bc1ab5203cc9fe
vishalk12345 <71704802+vishalk12345@users.noreply.github.com>
2020-10-06 16:58:18 +0530

    Update config.js

--- app/config.js
+++ app/config.js
@@ -1,26 +1,11 @@
 // Sets the MongoDB Database options
-
 module.exports = {
 
-    mongolab:
+    bitnami:
     {
-        name: "mongolab",
-        url: "mongodb://BigCoder:BigCoder!@ds139817.mlab.com:39817/heroku_cnkn5vpn",
-        port: 27017
-    },
-
-    local:
-    {
-        name: "scotch-user-map-local",
-        url: "mongodb://localhost/MeanMapApp",
-        port: 27017
-    },
-
-    localtest:
-    {
-        name: "scotch-user-map-local",
-        url: "mongodb://localhost/MeanMapAppTest",
-        port: 27017
+        name: "MongoDB Service",
+        url: "mongodb://" + process.env.DATABASE_USER + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT
     }
 
 };
